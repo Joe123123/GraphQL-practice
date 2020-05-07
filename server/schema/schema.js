7923ab1339f831b068d53f0658fb79355abd8a68
Zhao Wang <wangz2266@gmail.com>
2020-05-07 15:33:28 -0700

    add schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require("graphql");
+const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data
+const books = [
+  { name: "book111", id: "1", genre: "Fantasy" },
+  { name: "book22", id: "2", genre: "Fantasy" },
+  { name: "book3333", id: "3", genre: "Sci-Fi" },
+];
 
 const BookType = new GraphQLObjectType({
   name: "Book",
@@ -21,7 +29,12 @@ const RootQuery = new GraphQLObjectType({
       },
       resolve(parent, args) {
         // code to get data from db / other source
+        return _.find(books, { id: args.id });
       },
     },
   },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
 });
