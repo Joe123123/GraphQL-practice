83de799fadd3b34b84940365c9361c2abffe813f
Zhao Wang <wangz2266@gmail.com>
2020-05-09 11:43:05 -0700

    display books and apollo

--- client/src/components/BookList.js
+++ client/src/components/BookList.js
@@ -1,13 +1,33 @@
 import React, { Component } from "react";
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-export default class BookList extends Component {
+const getBookQuery = gql`
+  {
+    books {
+      name
+      id
+    }
+  }
+`;
+
+class BookList extends Component {
+  displayBooks = () => {
+    const data = this.props.data;
+    if (data.loading) {
+      return <div>Loading...</div>;
+    } else {
+      return data.books.map((book) => <li key={book.id}>{book.name}</li>);
+    }
+  };
+
   render() {
     return (
       <div>
-        <ul id="book-list">
-          <li>Book Name</li>
-        </ul>
+        <ul id="book-list">{this.displayBooks()}</ul>
       </div>
     );
   }
 }
+
+export default graphql(getBookQuery)(BookList);
