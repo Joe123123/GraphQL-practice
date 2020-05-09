d07a6388cbe30fd3fcd61df5a6122dc38001c2c6
Zhao Wang <wangz2266@gmail.com>
2020-05-09 14:56:02 -0700

    add book and refetch

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,24 +1,24 @@
-import React from "react";
-import { gql } from "apollo-boost";
-import { useQuery } from "@apollo/react-hooks";
+import React, { Component, useState } from "react";
+import { graphql } from "react-apollo";
+import { flowRight as compose } from "lodash";
+import { useMutation, useQuery } from "@apollo/react-hooks";
 
-const getAuthorsQuery = gql`
-  {
-    authors {
-      name
-      id
-    }
+import {
+  getAuthorsQuery,
+  addBookMutation,
+  getBookQuery,
+} from "../queries/queries";
+
+class AddBook extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", genre: "", authorId: "" };
   }
-`;
 
-export default function AddBook() {
-  const { loading, error, data } = useQuery(getAuthorsQuery);
-
-  const displayAuthors = (loading, error, data) => {
-    if (loading) {
-      return <option>Loading...</option>;
-    } else if (error) {
-      return <option>Error :(</option>;
+  displayAuthors() {
+    const data = this.props.getAuthorsQuery;
+    if (data.loading) {
+      return <option disabled>Loading...</option>;
     } else {
       return data.authors.map((author) => (
         <option key={author.id} value={author.id}>
@@ -26,28 +26,106 @@ export default function AddBook() {
         </option>
       ));
     }
-  };
+  }
 
-  return (
-    <div>
-      <form id="add-book">
-        <div className="field">
-          <label>Book Name:</label>
-          <input type="text" />
-        </div>
-        <div className="field">
-          <label>Genre:</label>
-          <input type="text" />
-        </div>
-        <div className="field">
-          <label>Authors:</label>
-          <select>
-            <option value="">select author</option>
-            {displayAuthors(loading, error, data)}
-          </select>
-        </div>
-        <button>+</button>
-      </form>
-    </div>
-  );
+  handleSubmit(e) {
+    e.preventDefault();
+    this.props.addBookMutation({
+      variables: { ...this.state },
+      refetchQueries: [{ query: getBookQuery }],
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form id="add-book" onSubmit={this.handleSubmit.bind(this)}>
+          <div className="field">
+            <label>Book Name:</label>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ name: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <label>Genre:</label>
+            <input
+              type="text"
+              onChange={(e) => this.setState({ genre: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <label>Authors:</label>
+            <select
+              onChange={(e) => this.setState({ authorId: e.target.value })}
+            >
+              <option value="">select author</option>
+              {this.displayAuthors()}
+            </select>
+          </div>
+          <button>+</button>
+        </form>
+      </div>
+    );
+  }
 }
+
+export default compose(
+  graphql(getAuthorsQuery, { name: "getAuthorsQuery" }),
+  graphql(addBookMutation, { name: "addBookMutation" })
+)(AddBook);
+
+// export default function AddBook() {
+//   const [state, setState] = useState({
+//     name: "",
+//     genre: "",
+//     authorId: "",
+//   });
+//   const [addBook] = useMutation(addBookMutation);
+//   const { loading, data } = useQuery(getAuthorsQuery);
+//   const displayAuthors = (loading, data) => {
+//     if (loading) {
+//       return <option disabled>Loading...</option>;
+//     } else {
+//       return data.authors.map((author) => (
+//         <option key={author.id} value={author.id}>
+//           {author.name}
+//         </option>
+//       ));
+//     }
+//   };
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     addBook({ variables: { ...state } });
+//   };
+//   return (
+//     <div>
+//       <form id="add-book" onSubmit={handleSubmit}>
+//         <div className="field">
+//           <label>Book Name:</label>
+//           <input
+//             type="text"
+//             onChange={(e) => setState({ ...state, name: e.target.value })}
+//           />
+//         </div>
+//         <div className="field">
+//           <label>Genre:</label>
+//           <input
+//             type="text"
+//             onChange={(e) => setState({ ...state, genre: e.target.value })}
+//           />
+//         </div>
+//         <div className="field">
+//           <label>Authors:</label>
+//           <select
+//             onChange={(e) => setState({ ...state, authorId: e.target.value })}
+//           >
+//             <option value="">select author</option>
+//             {displayAuthors(loading, data)}
+//           </select>
+//         </div>
+//         <button>+</button>
+//       </form>
+//     </div>
+//   );
+// }
